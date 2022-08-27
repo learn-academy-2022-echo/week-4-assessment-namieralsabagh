3af28c6a9398604b2fe165d11e9e5abedaaf43c3
namieralsabagh <namier@me.com>
2022-08-27 16:26:18 -0700

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -15,36 +15,114 @@
 
 // a) Create a test with an expect statement using the variable provided. 
 // HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
-
-const colors1 = ["purple", "blue", "green", "yellow", "pink"]
+describe("removeFirst", () =>{
+    const expected = ["purple", "blue", "green", "yellow", "pink"]
+    const expected1 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
+    it("takes in an array, removes the first item from the array and shuffles the remaining content", () =>{
+    const colors1 = ["purple", "blue", "green", "yellow", "pink"]
 // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
-const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
-// Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
+    const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
+// Expected output example (can be a different order): ["saffron", "aquamarine",        "periwinkle", "indigo", "ochre"]
+
+expect(removeFirst).not.toEqual(expect.arrayContaining(expected));
+expect(removeFirst).not.toEqual(expect.arrayContaining(expected1));
+    });
+});
+
+// ReferenceError: removeFirst is not defined
 
 
 // b) Create the function that makes the test pass.
+
+//PseudoCode
+// Declare a function called removeFirst
+// Set the parameter as an array
+// Declare a variable arrayShift and pass an empty argument
+// Index the array, use -1 to remove the first color using i = array.length -1; i > 0, i++
+
+
+    const removeFirst = (array) => {
+        const arrayShift = array.shift()
+         for (let i = array.length - 1; i > 0; i++) {
+            const k = Math.floor(Math.random() * (i+1));
+            const one = array [i];
+             array[i] = array [k]
+             array[k] = one;
+         }
+         return array 
+    }
+
+    // Tests: 1 passed, 1 total
 
 
 // --------------------2) Create a function that takes in an object that contains up votes and down votes and returns the net total of votes.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const votes1 = {upVotes: 13, downVotes: 2}
-// Expected output: 11
-const votes2 = {upVotes: 2, downVotes: 33}
-// Expected output: -31
+describe("totalVotes", ()=>{
+    it("takes in an object that contains up votes and down votes and returns the net total of votes", () =>{
+        const votes1 = {upVotes: 13, downVotes: 2}
+        // Expected output: 11
+        const votes2 = {upVotes: 2, downVotes: 33}
+        // Expected output: -31
+    expect(totalVotes(votes1)).toEqual[11]
+    expect(totalVotes(votes2)).toEqual[-31]
 
+    })
+})
+
+//ReferenceError: totalVotes is not defined
 
 // b) Create the function that makes the test pass.
+
+// PseudoCode
+// Declare a function called totalVotes
+// Set the parameter as object to take in the vote values 
+// Declare a variable theVotes that subtracts upvotes and downvotes
+// Return theVOtes
+
+
+
+    const totalVotes = (object) => {
+        let theVotes = (object.upVotes - object.downVotes)
+        return theVotes
+    };
+
+// Tests: 2 passed, 2 total
+
+
 
 
 // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
 
 // a) Create a test with an expect statement using the variables provided.
 
-const dataArray1 = ["array", "object", "number", "string", "Boolean"]
-const dataArray2 = ["string", "null", "Boolean", "string", "undefined"]
-// Expected output: ["array", "object", "number", "string", "Boolean", "null", "undefined"]
+describe("noDuplicates", () => {
+    it("takes in two arrays as arguments and returns one array with no duplicate values", () => {
+        const dataArray1 = ["array", "object", "number", "string", "Boolean"]
+        const dataArray2 = ["string", "null", "Boolean", "string", "undefined"]
+        // Expected output: ["array", "object", "number", "string", "Boolean", "null", "undefined"]
+        expect(noDuplicates(dataArray1, dataArray2)).toEqual(["array", "object", "number", "string", "Boolean", "null", "undefined"])
+    })
+})
+
+    // ReferenceError: noDuplicates is not defined
 
 
 // b) Create the function that makes the test pass.
+
+// PseudoCode
+// Decalre a function called noDuplicates
+// Set the parameters array1 and array2
+// Declare a variable called mergedArray = array1 and array2
+// Utilize the spread operator to combine arrays
+// Utilize Set() to create a new array with no duplicates
+// Utilize Array.from() to return the object into the array
+
+const noDuplicates = (array1, array2) => {
+    let mergedArray = [...array1, ...array2]
+    let setObject = new Set(mergedArray) 
+    return Array.from(new Set(mergedArray))
+}
+
+// Tests: 3 passed, 3 total
--- interview-questions.md
+++ interview-questions.md
@@ -8,42 +8,84 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is object-oriented programming? How is it different than functional programming?
 
-Your answer:
+Your answer: Object-oriented programming looks at everything as an object where functional programming uses syntax functions to create objects and classes. 
 
-Researched answer:
+Researched answer: Object-oriented programming was created to work with complex software systems.OOP looks at everything as an object. Ruby is an example of OOP. OOP's data is mutable and is not suited for Parallel Programming. The flow of OOP is controlled using loops and conditonal statements. Functional programming is different becuase it utilizes immutable data that follows the declartive programming model where OOP follows the imperative programming model.  
 
 2. What is the difference between a Float and an Integer in Ruby?
 
-Your answer:
+Your answer: Float is a number decimal like 2.7 and interger is a whole number like 3. Float has to be passed into an equation if you don't want only a whole number to return.
 
-Researched answer:
+Researched answer: Intergers are whole numbers and floats are non-whole numbers. You would use a float if you wanted to reutrn a non-whole number or a number with a remainder. An example of float is;
+
+9.0 / 8
+This would return 1.125
 
 3. Ruby has an implicit return. What does that mean?
 
-Your answer:
+Your answer: An implicit return is when no return is specified it returns the value of the last line is automatically. An example is;
 
-Researched answer:
+def name
+    'Hello Namier'
+end
+    p name would return 'Hello Namier'
+    puts name would return Hello Namier
+
+Researched answer: An implicit reutrn is a returned value without using the keyword return in your syntax. This is commonly utilized in Ruby. An example of implicit return in Ruby syntax is;
+
+def name
+    'Hello Charlean and Austin!'
+end
+    p name would return 'Hello Charlean and Austin!'
+    puts name would return Hello Charlean and Austin!
 
 4. What is a block in Ruby?
 
-Your answer:
+Your answer: A block in ruby is code that doesn't require a name. It is an anonymous function and it is stored between do/end. 
 
-Researched answer:
+Researched answer: Ruby block syntax is an annonymous function that utlizes the  do/end statement. These blocks determine the outcome of the method that is is passed to. An example of Ruby block using the times method is;
+
+my_num = 3
+
+my_num.times do
+    puts 'Echo 2022'
+end
+
+This would print;
+
+Echo 2022
+Echo 2022
+Echo 2022
 
 5. What is an instance variable in Ruby?
 
-Your answer:
+Your answer: An instance variable in ruby starts with @ and belongs to the instance of the class. It is accessed in the local scope. 
 
-Researched answer:
+Researched answer: Instance variables are locally scoped and can be accessed using using the accessor methods set and get in the class its scoped in. In order for an instance varaible to be accessable, it must contain the @ symbol in front of it. Instance variables cannot be accessed from outside of an object. Instance variables will not have a value before they are initialized. Instance variables do not require a declaration for flexability in structing objects. An example of a class with an instance variable is;
+
+class Superhero
+    def set_name(name)
+         @name = name
+    end
+
+    def get_name
+        @name
+    end
+ end
+
+ p superhero_one = Superhero.new (This is the new instance)
+ superhero_one.set_name('Batman') (This is setting the superhero name)
+ p superhero_one.get_name (This is getting the new instance)
+ would output 'Batman'
 
 ## Looking Ahead: Terms for Next Week
 
-1. PostgreSQL:
+1. PostgreSQL: is an object-relational databse management system. It is open source and has over 30 years of active development. It is known for its reliability. 
 
-2. Ruby on Rails:
+2. Ruby on Rails: is a server-side web application framework that is written in Ruby. It was created by David Heinemeier Hansson (DHH) in July 2004. It is open source. 
 
-3. ORM:
+3. ORM: Object-relational mapping is a programming technique for converting data between incompatible systems using object-oriented programming languages. It's the tool that lets develipers write SQL queries using ruby on rails. 
 
-4. Active Record:
+4. Active Record: is a orm that takes data that is stored in a datbse table and lets you interact with the data like a Ruby object. 
 
-5. Migrations:
+5. Migrations: are a tool that allows you to change that shape of your database over time. They are files that run SQL commands.
